06822abc65809d44098617637bb402e79759df35
Maxazure <maxazure@gmail.com>
2021-06-15 12:36:24 +1200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created(){
+    document.title = "后台管理系统模板"
+  }
+}
+</script>
